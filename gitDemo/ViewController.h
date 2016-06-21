8771a164c9a8b09e9c4e03785807b3165e840605
朱慧平 <1429335626@qq.com>
2016-06-21 15:12:51 +0800

    create

---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by 朱慧平 on 16/6/21.
+//  Copyright © 2016年 Crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitDemo/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by 朱慧平 on 16/6/21.
+//  Copyright © 2016年 Crystal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [[UIButton alloc] initWithFrame:self.view.bounds];
+    button.backgroundColor = [UIColor redColor];
+    [button setTitle:@"Touch Button" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(alertViewShow) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+- (void)alertViewShow{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Tip" message:@"This is a test abou git " delegate:self cancelButtonTitle:@"I Know" otherButtonTitles:nil, nil];
+    NSLog(@"hahahahahah");
+    [alert show];
+//    i am not okey
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
